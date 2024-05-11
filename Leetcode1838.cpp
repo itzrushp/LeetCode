5a104151b57ed091484ebe0c3ff4e7ea595ac21c
Roshan Pandit <123315509+itzrushp@users.noreply.github.com>
2024-05-11 22:31:28 +0530

    Create Leetcode1838.cpp
    (C++) 1838. Frequency of the Most Frequent Element

---
+++ Leetcode1838.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int maxFrequency(vector<int>& nums, int k) {
+    int ans = 0;
+    long sum = 0;
+
+    sort(nums);
+
+    for (int l = 0, r = 0; r < nums.size(); ++r) {
+      sum += nums[r];
+      while (sum + k < static_cast<long>(nums[r]) * (r - l + 1))
+        sum -= nums[l++];
+      ans = max(ans, r - l + 1);
+    }
+
+    return ans;
+  }
+};
+
+
+/*
+<  ASKED IN FACEBOOK , AMAZON interveiw Questions />
+So let us look at the logic behind it :
+
+
+
+*/
