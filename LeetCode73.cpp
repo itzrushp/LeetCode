d3636f6700bf0f8cfde8daa5e7989dea155dccbf
Roshan Pandit <123315509+itzrushp@users.noreply.github.com>
2024-07-09 01:47:26 +0530

    Create LeetCode73.cpp

---
+++ LeetCode73.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<bool> zeroRows(rows, false);
+        vector<bool> zeroCols(cols, false);
+
+        // First pass: mark the rows and columns that need to be zeroed
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows[i] = true;
+                    zeroCols[j] = true;
+                }
+            }
+        }
+
+        // Second pass: set the rows and columns to zero
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (zeroRows[i] || zeroCols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
